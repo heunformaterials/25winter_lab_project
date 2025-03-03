9c03b7cb2049cdf57cd86e7a401f6701ace00d83
seanjongheunkim <olympic1234@snu.ac.kr>
2025-03-03 12:34:49 +0900

    new javascript project

---
+++ javascript_train/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 기본 라우트 설정
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- javascript_train/winter_lab_intern01.js
+++ javascript_train/winter_lab_intern01.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 app.use(express.static("public"))
 
